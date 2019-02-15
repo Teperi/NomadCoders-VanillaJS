2802e8897e9391068aeaf678c80e79fa2fb39790
Teperi Lee <teperi31@gmail.com>
2019-02-15 13:41:39 +0900

    사용자 이름 저장기능

--- #3/clock.js
+++ #3/clock.js
@@ -9,7 +9,7 @@ function getTime() {
     clockTitle.innerText = `${
         hours < 10 ? `0${hours}` : hours
     }:${
-        minutes < 10? `0${minutes}` : minutes
+        minutes < 10 ? `0${minutes}` : minutes
     }:${
         seconds < 10 ? `0${seconds}` : seconds
     }`;
